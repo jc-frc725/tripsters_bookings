0b0e9df02c99e11f82a74964e6686b2b4ebaed82
Justin Chow <hrll71990@gmail.com>
2021-02-06 10:19:27 -0800

    changed booking generation to make 20M booking records instead of 40M due to memory heap problems while generating

--- newServer/database/generateProperties.js
+++ newServer/database/generateProperties.js
@@ -8,13 +8,16 @@ const stream = fs.createWriteStream(destination);
 stream.write('price,cleaning,avg,totalRev\n');
 let propertyId = 1;
 
-while (propertyId <= 1000) {
+while (propertyId <= 10000000) {
   const price = faker.random.number({ min: 100, max: 300 });
   const cleaning = Math.floor(faker.random.number({ min: 100, max: 250 }) * 0.12);
   const avg = faker.random.float({ min: 2, max: 5 });
   const totalRev = faker.random.number({ min: 40, max: 250 });
   const entry = `${price},${cleaning},${avg},${totalRev}\n`;
   stream.write(entry);
+  if (propertyId % 100000 === 0) {
+    console.log(`progress: ${propertyId / 100000}%`);
+  }
   propertyId += 1;
 }
 
