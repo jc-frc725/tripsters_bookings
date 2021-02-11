31e20e791be406cacca3135b524356607fe26ab9
Justin Chow <hrll71990@gmail.com>
2021-02-10 22:58:30 -0800

    configuring server index for optimizations

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
 require('dotenv').config();
 require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
-const compression = require('compression');
+//const morgan = require('morgan');
+//const compression = require('compression');
+const controller = require('controller');
 const path = require ('path');
 
 const PUBLIC_DIR = path.resolve(__dirname, '../client/dist');
-const router = require('./routes');
+//const router = require('./routes');
 const db = require('./database');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(compression());
-app.use(express.static(PUBLIC_DIR));
+// app.use(morgan('dev'));
+// app.use(express.json());
+// app.use(compression());
+// app.use(express.static(PUBLIC_DIR));
 
-app.use('/api/rooms/', router);
+// app.use('/api/rooms/', router);
+app.get('/api/rooms/:propertyId/booking/calendar', controller.getCal);
+app.get('/api/rooms/:propertyId/booking', controller.getProp)
+
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
